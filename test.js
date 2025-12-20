2e76d0b76e4e944d4b73bd9278e932ad5574db24
𝗔𝗿𝗮𝗳𝗮𝘁 𝘀𝗮𝗿𝗱𝗲𝗿 <arafatas206602@gmail.com>
2025-12-20 12:53:33 +0600

    Update test.js

--- test.js
+++ test.js
@@ -102,7 +102,7 @@ async function makeRankCard(userID, usersData, threadsData, threadID, deltaNext,
 		name: allUser[rank - 1].name,
 		rank: `#${rank}/${allUser.length}`,
 		level: levelUser,
-		avatar: `https://graph.facebook.com/${userID}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`
+		avatar: `https://graph.facebook.com/${userID}/picture?width=720&height=720&redirect=true&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`
 	};
 
 	const configRankCard = {
